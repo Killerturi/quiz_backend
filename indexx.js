697fc07528bacf3a31a2b47dce1af2fd0c18807f
test <killerTuri@gmail.com>
2026-04-30 11:32:39 +0530

    add frontend url

--- indexx.js
+++ indexx.js
@@ -22,7 +22,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://192.168.0.105:5173"
+  "https://youthquiz.netlify.app"
 ];
 
 app.use(cors({
